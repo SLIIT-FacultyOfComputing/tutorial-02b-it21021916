006f8de716a07e00e5d6ce09afc257ad29e1881f
IT21021916 Rashmi <87628868+it21021916@users.noreply.github.com>
2021-07-26 07:14:51 +0000

    I have completed exercise_02

--- tute02.cpp
+++ tute02.cpp
@@ -12,33 +12,40 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
-int main()
+#include <iostream>
+using namespace std;
+int main()//function main begins
 {
-   double salary, netSalary;
-   int etype, otHrs, otRate;
-   printf("Enter Employee Type : ");
-   scanf("%d", &etype);
-   printf("Enter Salary  : ");
-   scanf("%f", &salary);
-   printf("Enter OtHrs : ");
-   scanf("%d", &otHrs); 
+
+double salary, netSalary;
+int etype, otHrs, otRate;
+
+cout<<"Enter Employee Type : ";//prompt
+cin>>etype;//read an integer
+
+cout<<"Enter Salary  : ";//prompt
+cin>>salary;//read a float value
+
+cout<<"Enter OtHrs : ";//prompt
+cin>>otHrs;//read an integer 
    
-   switch (etype) {
-      case 1 :
-          otRate = 1000;
-          break;
-      case 2 :
-          otRate = 1500;
-          break;
-      default :
-          otRate = 1700;
-          break;
-   }
+switch (etype)
+{
+case 1 :
+otRate = 1000;
+break;
+case 2 :
+otRate = 1500;
+break;
+default :
+otRate = 1700;
+break;
+}
 
+netSalary = salary + otHrs* otRate;//net salary will be stored
 
-   netSalary = salary + otHrs* otRate;
-   printf("Net Salary is %f ", netSalary);
+cout<<"Net Salary is : "<<netSalary<<endl;//print net salary
   
-   return 0;
+return 0;//end of the main function
+
 }
